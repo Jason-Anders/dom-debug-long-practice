33e4053d9ac127f8e6436b9ad5479fbfbbc8a6e6
Jason-Anders <jason.charles.anders@gmail.com>
2025-04-01 14:40:08 -0400

    phase bonus complete

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,33 +1,34 @@
 export function findElementById(id) {
     // Return the element in the DOM with corresponding `id`
-    const allKids = document.body.children
-    for (const kid of allKids) {
-        if (kid.id===id){return kid}
-    }
-    return null
+
+
+    return document.getElementById(id);
     // Your code here
 }
 
 export function findFirstElementOfTag(tag) {
     // Return the first occurence of an element of tag name `tag`
+const tags = document.getElementsByTagName(tag);
+return tags[0];
 
-    // Your code here
 }
 
 export function findFirstElementOfClass(cls) {
     // Return the first occurence of an element of class `cls`
-
+const clss = document.getElementsByClassName(cls);
+return clss[0];
     // Your code here
 }
 
 export function findElementsOfTag(tag) {
     // Return an array of elements that have a tag name of `tag`
-
+const tags = document.getElementsByTagName(tag);
+return tags;
     // Your code here
 }
 
 export function findElementsOfClass(cls) {
     // Return an array of elements that have are of class `cls`
-
+return document.getElementsByClassName(cls);
     // Your code here
 }
